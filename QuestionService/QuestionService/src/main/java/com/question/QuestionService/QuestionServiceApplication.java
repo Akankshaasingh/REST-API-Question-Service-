35fb242a76f359172265eb56e62e14c24dbf0a5d
AKANKSHA SINGH <90822445+Akankshaasingh@users.noreply.github.com>
2024-06-20 22:40:40 +0530

    Add files via upload

---
+++ QuestionService/QuestionService/src/main/java/com/question/QuestionService/QuestionServiceApplication.java
@@ -0,0 +1,18 @@
+package com.question.QuestionService;
+
+import com.question.QuestionService.entities.Question;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class QuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionServiceApplication.class, args);
+	}
+
+
+
+}
